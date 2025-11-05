84988fdb3f2049a6df345945971df65f6a696f4c
usc-c-r137 <c_r137@student.usc.edu.au>
2025-11-05 16:48:19 +1000

    js testing

--- map.js
+++ map.js
@@ -4,7 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
         return;
     }
 
+    const infoCardOne = document.getElementById('info-card-one');
+    const infoCardTwo = document.getElementById('info-card-two');
+    const infoCardThree = document.getElementById('info-card-three');
+    const infoCardFour = document.getElementById('info-card-four');
+
     var map = L.map('map').setView([-27.46860, 153.02230], 13);
+
+    infoCardOne.textContent = 'Test One';
+    infoCardTwo.textContent = 'Test Two';
+    infoCardThree.textContent = 'Test Three';
+    infoCardFour.textContent = 'Test Four';
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
